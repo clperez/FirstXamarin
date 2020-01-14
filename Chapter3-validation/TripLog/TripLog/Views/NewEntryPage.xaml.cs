331983980dbd5a56549f15b8c16b624b129e87c8
Carlos <carlillos34@gmail.com>
2020-01-14 20:55:02 +0100

    Adding the project to check how to validate with Xamarin

---
+++ Chapter3-validation/TripLog/TripLog/Views/NewEntryPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TripLog.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TripLog.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewEntryPage : ContentPage
+    {
+        public NewEntryPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new NewEntryViewModel();
+
+        }
+    }
+}
