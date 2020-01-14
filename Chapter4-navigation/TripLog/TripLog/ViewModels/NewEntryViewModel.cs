685d6dcabe9b252afb58b48d4c20c8f88606a2a4
Carlos <carlillos34@gmail.com>
2020-01-14 22:38:12 +0100

    Adding a project to perform navigation on model view controllers.

---
+++ Chapter4-navigation/TripLog/TripLog/ViewModels/NewEntryViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TripLog.Models;
+using Xamarin.Forms;
+
+namespace TripLog.ViewModels
+{
+    public class NewEntryViewModel : BaseValidationViewModel
+    {
+        public NewEntryViewModel()
+        {
+            Date = DateTime.Today;
+            Rating = 1;
+        }
+
+        string _title;
+        public string Title {
+            get => _title;
+            set
+            {
+                _title = value;
+                Validate(() => !string.IsNullOrWhiteSpace(_title), "Title must be provided.");
+                OnPropertyChanged();
+                SaveCommand.ChangeCanExecute();
+            }
+        }
+        double _latitude;
+        public double Latitude {
+            get => _latitude;
+            set
+            {
+                _latitude = value;
+                OnPropertyChanged();
+            }
+        }
+        double _longitude;
+        public double Longitude {
+            get => _longitude;
+            set
+            {
+                _longitude = value;
+                OnPropertyChanged();
+            }
+        }
+        DateTime _date;
+        public DateTime Date {
+            get => _date;
+            set
+            {
+                _date = value;
+                OnPropertyChanged();
+            }
+        }
+        int _rating;
+        public int Rating {
+            get => _rating;
+            set
+            {
+                _rating = value;
+                Validate(() => _rating >= 1 && _rating <= 5, "Rating must be between 1 and 5.");
+                OnPropertyChanged();
+                SaveCommand.ChangeCanExecute();
+            }
+        }
+        string _notes;
+        public string Notes {
+            get => _notes;
+            set
+            {
+                _notes = value;
+                OnPropertyChanged();
+            }
+        }
+
+        Command _saveCommand;
+        public Command SaveCommand => _saveCommand ?? (_saveCommand = new Command(Save, CanSave));
+
+        void Save()
+        {
+            var newItem = new TripLogEntry
+            {
+                Title = Title,
+                Latitude = Latitude,
+                Longitude = Longitude,
+                Date = Date,
+                Rating = Rating,
+                Notes = Notes
+            };
+            // TODO: Persist entry in a later chapter
+        }
+        bool CanSave() => !string.IsNullOrWhiteSpace(Title) && !HasErrors;
+
+    }
+}
---
+++ Chapter4-navigation/TripLog/TripLog/Views/DetailPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TripLog.Models;
+using TripLog.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Xaml;
+
+namespace TripLog.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailPage : ContentPage
+    {
+        DetailViewModel ViewModel => BindingContext as DetailViewModel;
+
+        public DetailPage(TripLogEntry entry)
+        {
+            InitializeComponent();
+            BindingContext = new DetailViewModel(entry);
+            map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(ViewModel.Entry.Latitude, ViewModel.Entry.Longitude),Distance.FromMiles(.5)));
+            map.Pins.Add(new Pin
+            {
+                Type = PinType.Place,
+                Label = ViewModel.Entry.Title,
+                Position = new Position(ViewModel.Entry.Latitude, ViewModel.Entry.Longitude)
+            });
+        }
+    }
+}
