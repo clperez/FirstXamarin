b4470165efb2edeb7f41722c241c3b5984eec1d3
Carlos <carlillos34@gmail.com>
2020-01-22 00:48:10 +0100

    Reverting all to starting position after navigation

---
+++ Current/TripLog/TripLog.Android/Services/LocationService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TripLog.Models;
+using TripLog.Services;
+
+namespace TripLog.Android.Services
+{
+    public class LocationService : ILocationService
+    {
+        public async Task<GeoCoords> GetGeoCoordinatesAsync()
+        {
+            var location = await Xamarin.Essentials.Geolocation.GetLocationAsync();
+            return new GeoCoords
+            {
+                Latitude = location.Latitude,
+                Longitude = location.Longitude
+            };
+        }
+    }
+}
---
+++ Current/TripLog/TripLog.iOS/Modules/TripLogPlatformModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using Ninject.Modules;
+using TripLog.iOS.Services;
+using TripLog.Services;
+using UIKit;
+
+namespace TripLog.iOS.Modules
+{
+    public class TripLogPlatformModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ILocationService>().To<LocationService>().InSingletonScope();
+        }
+    }
+}
