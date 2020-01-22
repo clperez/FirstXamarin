abef99638c76c720505e43b49e650ba1cfb18573
Carlos <carlillos34@gmail.com>
2020-01-23 00:08:39 +0100

    Control Renderers

---
+++ Chapter6a-control-renderers/TripLog/TripLog/Controls/DatePickerEntryCell.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TripLog.Controls
+{
+    public class DatePickerEntryCell : EntryCell
+    {
+        public static readonly BindableProperty DateProperty =
+        BindableProperty.Create(
+           nameof(Date),
+           typeof(DateTime),
+           typeof(DatePickerEntryCell),
+           DateTime.Now,
+           BindingMode.TwoWay);
+        public DateTime Date {
+            get => (DateTime)GetValue(DateProperty);
+            set => SetValue(DateProperty, value);
+        }
+
+        public DatePickerEntryCell()
+        {
+        }
+    }
+}
