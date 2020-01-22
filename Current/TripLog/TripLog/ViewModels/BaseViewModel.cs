7a51d22f026a429c005fb3c5488f385d20295d86
Carlos <carlillos34@gmail.com>
2020-01-22 19:04:46 +0100

    Here is the thing.

---
+++ Current/TripLog/TripLog/ViewModels/BaseViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using TripLog.Services;
+
+namespace TripLog.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void Init()
+        {
+
+        }
+
+        // Called without arguments. The argument is added by the compiler.
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected INavService NavService { get; private set; }
+        protected BaseViewModel(INavService navService)
+        {
+            NavService = navService;
+        }
+    }
+
+    public class BaseViewModel<TParameter> : BaseViewModel
+    {
+        public override void Init()
+        {
+            Init(default(TParameter));
+        }
+        public virtual void Init(TParameter parameter)
+        {
+
+        }
+
+        protected BaseViewModel(INavService navService)
+        : base(navService)
+        {
+        }
+    }
+}
---
+++ Current/TripLog/TripLog/Views/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TripLog.Models;
+using TripLog.Services;
+using TripLog.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TripLog.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        MainViewModel ViewModel => BindingContext as MainViewModel;
+        // ...
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            // Initialize MainViewModel
+            ViewModel?.Init();
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel(DependencyService.Get<INavService>());
+        }
+    }
+}
