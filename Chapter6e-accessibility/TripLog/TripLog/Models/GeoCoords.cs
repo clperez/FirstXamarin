b7b23e83971ccbe4fc26bb5ddaa1f6c587c96903
Carlos <carlillos34@gmail.com>
2020-01-23 23:57:13 +0100

    Adding accessibility reading capabilities.

---
+++ Chapter6e-accessibility/TripLog/TripLog/Models/GeoCoords.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TripLog.Models
+{
+    public class GeoCoords
+    {
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+
+    }
+}
