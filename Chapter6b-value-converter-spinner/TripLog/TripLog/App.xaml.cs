3335abc59758bf7289f38fb2a203b75130488ba6
Carlos <carlillos34@gmail.com>
2020-01-23 23:06:50 +0100

    Value converters spinner waiting added.

---
+++ Chapter6b-value-converter-spinner/TripLog/TripLog/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Ninject;
+using Ninject.Modules;
+using System;
+using TripLog.Modules;
+using TripLog.Services;
+using TripLog.ViewModels;
+using TripLog.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TripLog
+{
+    public partial class App : Application
+    {
+        public IKernel Kernel { get; set; }
+
+        public App(params INinjectModule[] platformModules)
+        {
+            InitializeComponent();
+            Kernel = new StandardKernel( new TripLogCoreModule(), new TripLogNavModule());
+            Kernel.Load(platformModules);
+            
+            // NavigationPage. To get navigation components between pages
+            var mainPage = new NavigationPage(new MainPage());
+            mainPage.BindingContext = Kernel.Get<MainViewModel>();
+
+            //var navService = DependencyService.Get<INavService>() as XamarinFormsNavService;
+            var navService = Kernel.Get<INavService>() as XamarinFormsNavService;
+            navService.XamarinFormsNav = mainPage.Navigation;
+            
+            MainPage = mainPage;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Chapter6b-value-converter-spinner/TripLog/TripLog/Modules/TripLogCoreModule.cs
@@ -0,0 +1,18 @@
+﻿using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TripLog.ViewModels;
+
+namespace TripLog.Modules
+{
+    public class TripLogCoreModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<MainViewModel>().ToSelf();
+            Bind<DetailViewModel>().ToSelf();
+            Bind<NewEntryViewModel>().ToSelf();
+        }
+    }
+}
