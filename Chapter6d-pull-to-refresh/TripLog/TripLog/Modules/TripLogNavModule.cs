71e705f158ad2d5bc9b403f2e86368efc6fa12f0
Carlos <carlillos34@gmail.com>
2020-01-23 23:49:18 +0100

    Adding pull to refresh control.

---
+++ Chapter6d-pull-to-refresh/TripLog/TripLog/Modules/TripLogNavModule.cs
@@ -0,0 +1,24 @@
+﻿using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TripLog.Services;
+using TripLog.ViewModels;
+using TripLog.Views;
+
+namespace TripLog.Modules
+{
+    class TripLogNavModule : NinjectModule
+    {
+        public override void Load()
+        {
+            var navService = new XamarinFormsNavService();
+            navService.RegisterViewMapping(typeof(MainViewModel), typeof(MainPage));
+            navService.RegisterViewMapping(typeof(DetailViewModel), typeof(DetailPage));
+            navService.RegisterViewMapping(typeof(NewEntryViewModel), typeof(NewEntryPage));
+            Bind<INavService>()
+                .ToMethod(x => navService)
+                .InSingletonScope();
+        }
+    }
+}
