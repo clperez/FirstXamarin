f56a084599d753ad25d49cc582e6957e283ac714
Carlos <carlillos34@gmail.com>
2020-01-23 23:37:33 +0100

    Adding number to image converter to show stars.

---
+++ Chapter6c-value-converter-images/Qwantalabs.Shared/Functional.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Qwantalabs.Shared
+{
+    public static class Functional
+    {
+        public static TInput If<TInput>(this TInput input, Func<TInput, bool> evaluator) {
+            if (Object.Equals(input, default(TInput))) return default;
+            return evaluator(input) ? input : default;
+        }
+        
+        // Project a type into another
+        public static TResult Select<TInput, TResult>(this TInput input, Func<TInput, TResult> evaluator, TResult failureValue)
+        {
+            if (Object.Equals(input, default(TInput))) return failureValue;
+            return evaluator(input);
+        }
+        
+        // Project a type into another
+        public static TResult Select<TInput, TResult>(this TInput input, Func<TInput, TResult> evaluator) =>
+            input.Select(evaluator, default);
+
+        // This one can be concatenated. Many Do Do Do Do
+        public static TInput Do<TInput>(this TInput input, Action<TInput> action){
+            if (Object.Equals(input, default(TInput))) return default;
+            action(input);
+            return input;
+        }
+    }
+
+
+    public static class FunctionalAsync
+    {
+        // Project a type into another
+        public static async Task<TResult> SelectAsync<TInput, TResult>(this TInput input, Func<TInput, TResult> evaluator, ValueTask<TResult> failureValue) =>
+            Object.Equals(input, default(TInput)) ? await failureValue : evaluator(input);
+
+        // Project a type into another
+        public static async Task<TResult> SelectAsync<TInput, TResult>(this TInput input, Func<TInput, TResult> evaluator) =>
+            await input.SelectAsync(evaluator, default);
+
+        // This one can be concatenated. Many Do Do Do Do
+        public static async Task<TInput> DoAsync<TInput>(this TInput input, Func<TInput,Task> action)
+        {
+            if (Object.Equals(input, default(TInput))) return default;
+            await action(input);
+            return input;
+        }
+    }
+}
---
+++ Chapter6c-value-converter-images/TripLog/TripLog/Converters/RatingToStarImageNameConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TripLog.Converters
+{
+    public class RatingToStarImageNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int rating)
+            {
+                if (rating <= 1)
+                {
+                    return "star_1";
+                }
+                if (rating >= 5)
+                {
+                    return "stars_5";
+                }
+                return "stars_" + rating;
+            }
+            return value;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
