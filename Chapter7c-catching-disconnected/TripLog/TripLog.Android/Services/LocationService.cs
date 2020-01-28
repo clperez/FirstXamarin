92672227ef9db7462d11f4dbc4a14ee89e682c59
Carlos L. Perez <carlillos34@gmail.com>
2020-01-28 16:46:57 +0100

    Caching disconnected service.

---
+++ Chapter7c-catching-disconnected/TripLog/TripLog.Android/Services/LocationService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using TripLog.Models;
+using TripLog.Services;
+
+namespace TripLog.Droid.Services
+{
+    public class LocationService : ILocationService
+    {
+        public async Task<GeoCoords> GetGeoCoordinatesAsync()
+        {
+            var location = await Xamarin.Essentials.Geolocation.GetLocationAsync();
+            return new GeoCoords
+            {
+                Latitude = location.Latitude,
+                Longitude = location.Longitude
+            };
+        }
+    }
+}
---
+++ Chapter7c-catching-disconnected/TripLog/TripLog/Models/TripLogEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TripLog.Models
+{
+    public class TripLogEntry
+    {
+        public string Title { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public DateTime Date { get; set; }
+        public int Rating { get; set; }
+        public string Notes { get; set; }
+    }
+}
